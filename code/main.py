d40312b85ca08a97f50e8faa03cdd866c03d1681
Agustin841155 <1719110184@utectulancingo.edu.mx>
2022-06-10 20:52:26 +0000

    90% de tarea completada

---
+++ code/main.py
@@ -0,0 +1,24 @@
+from fastapi import FastAPI
+import sqlite3
+from typing import List 
+from pydantic import BaseModel
+
+class Respuesta(BaseModel):
+    message: str
+
+
+
+app=FastAPI()
+
+@app.get("/", response_model=Respuesta)
+async def index():
+    return{"message": "Hello world"}
+
+@app.get("/clientes/")
+async def clientes():
+    with sqlite3.connect("sql/clientes.sqlite") as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM clientes") 
+        response = cursor.fetchall()
+        return response 
