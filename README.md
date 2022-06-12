36d740bd40d3c583b7cc31d2ce011a678423dd31
Agustin841155 <1719110184@utectulancingo.edu.mx>
2022-06-12 18:11:19 +0000

    api rest funcinando

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # API-REST
 CREACION DE API REST Y SU USO
+## creacion de imagen 
+docker build -t imagen_api:v1 .
+
+## creacion de contenedor basado en imagen
+
+docker run -it - v "PWD"/home:/home/code --net=host --name api_rest -h agustin imagen_api:v1
--- code/main.py
+++ code/main.py
@@ -6,13 +6,16 @@ from pydantic import BaseModel
 class Respuesta(BaseModel):
     message: str
 
-
+class Cliente(BaseModel):
+    id_cliente: int
+    nombre: str
+    email: str
 
 app=FastAPI()
 
 @app.get("/", response_model=Respuesta)
 async def index():
-    return{"message": "Hello world"}
+    return{"message": "API REST"}
 
 @app.get("/clientes/")
 async def clientes():
@@ -20,5 +23,14 @@ async def clientes():
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM clientes") 
+        response = cursor.fetchall()
+        return response 
+
+@app.get("/clientes/{id}")
+async def clientes(id):
+    with sqlite3.connect("sql/clientes.sqlite") as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM clientes WHERE id_cliente={}".format(int(id))) 
         response = cursor.fetchall()
         return response 
