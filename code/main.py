170c5225850ee73d3070223e29ea54090b4a1372
Agustin841155 <1719110184@utectulancingo.edu.mx>
2022-06-21 04:49:00 +0000

    api rest parte 2

--- code/main.py
+++ code/main.py
@@ -11,26 +11,62 @@ class Cliente(BaseModel):
     nombre: str
     email: str
 
+class ClienteIN(BaseModel):
+    nombre:str
+    email:str
+
 app=FastAPI()
 
 @app.get("/", response_model=Respuesta)
 async def index():
     return{"message": "API REST"}
 
-@app.get("/clientes/")
-async def clientes():
+@app.get("/clientes/",response_model=List[Cliente])
+async def clientes(offset:int=0, limit:int=10):
     with sqlite3.connect("code/sql/clientes.sqlite") as connection:
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
-        cursor.execute("SELECT * FROM clientes") 
+        cursor.execute("SELECT * FROM clientes LIMIT ? OFFSET ?", (limit,offset)) 
         response = cursor.fetchall()
         return response 
 
 @app.get("/clientes/{id}")
-async def clientess(id):
-    with sqlite3.connect("sql/clientes.sqlite") as connection:
+async def clientes_id(id):
+    with sqlite3.connect("code/sql/clientes.sqlite") as connection:
         connection.row_factory = sqlite3.Row
         cursor = connection.cursor()
         cursor.execute("SELECT * FROM clientes WHERE id_cliente={}".format(int(id))) 
         response = cursor.fetchall()
         return response 
+
+#Parte 2
+
+@app.post("/clientes/",response_model=Respuesta) 
+async def post_cliente(nombre:str,email:str): 
+    with sqlite3.connect("code/sql/clientes.sqlite") as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("INSERT INTO clientes(nombre,email) VALUES ('{}','{}')".format(nombre,email))
+        response = cursor.fetchone()
+        mensaje = {"message" : "Cliente agregado"}
+        return mensaje
+
+@app.put("/clientes/{id_cliente}",response_model=Respuesta)
+async def put_cliente(id_cliente:int,nombre:str,email:str):
+    with sqlite3.connect("code/sql/clientes.sqlite") as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("UPDATE clientes SET nombre = '{}', email = '{}' WHERE id_cliente={}".format(nombre,email,id_cliente))
+        response = cursor.fetchone()
+        mensaje = {"message" : "Cliente actualizado"}
+        return mensaje       
+
+@app.delete("/clientes/{id_cliente}",response_model=Respuesta)
+async def delete_cliente(id_cliente:int):
+   with sqlite3.connect("code/sql/clientes.sqlite") as connection:
+        connection.row_factory = sqlite3.Row
+        cursor = connection.cursor()
+        cursor.execute("DELETE FROM clientes WHERE id_cliente={}".format(id_cliente))
+        response = cursor.fetchone()
+        mensaje = {"message" : "Cliente borrado"}
+        return mensaje
--- test/test_main.py
+++ test/test_main.py
@@ -1,5 +1,5 @@
 from fastapi.testclient import TestClient
-
+import json
 
 from code.main import app
 
@@ -15,3 +15,29 @@ def test_clientes():
     assert response.status_code == 200
     assert response.json() == [{"id_cliente":1,"nombre":"Dejah","email":"dejah@gmail.com"},
     {"id_cliente":2,"nombre":"John","email":"john@gmail.com"}, {"id_cliente":3,"nombre":"Carthoris","email":"carthoris@gmail.com"}]
+    
+def test_clientes_id():
+    response = clientes.get('/clientes/1')
+    data = [{"id_cliente":1,"nombre":"Dejah","email":"dejah@gmail.com"}]
+    assert response.status_code == 200
+    assert response.json() == data
+
+def test_post_cliente():
+    payload = {"id_cliente":4,"nombre":"agus", "email": "agus@gmail.com"}
+    response = clientes.post("/clientes/", json=payload) 
+    Response = {"message" : "Cliente agregado"}
+    assert response.status_code == 200
+    assert response.json() == Response
+
+def test_put_cliente():
+    payload = {"id_cliente":4,"nombre" : "Agustin", "email" : "agustin@gmail.com"}
+    response = clientes.put("/clientes/",json=payload)
+    Response = {"message" : "Cliente actualizado"}
+    assert response.status_code == 200
+    assert response.json() == Response
+
+def test_delete_cliente():
+    response = clientes.delete("/clientes/4")
+    Response = {"message" : "Cliente borrado"}
+    assert response.status_code == 200
+    assert response.json() == Response
